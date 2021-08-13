d5d707821fd0576c036da0dc1e8f4331c0641466
= <Jay.n.damon@gmail.com>
2021-08-13 14:56:01 -0400

    feat: Shrink photo sizes

--- app/index.js
+++ app/index.js
@@ -1,16 +1,3 @@
-// let menu = document.getElementById("menu-component");
-// let htmlFile = new XMLHttpRequest();
-// htmlFile.open("GET", "/core/menu.html", false);
-// htmlFile.onreadystatechange = function () {
-//     if (htmlFile.readyState === 4) {
-//         if (htmlFile.status === 200 || htmlFile.status == 0) {
-//             var allText = htmlFile.responseText;
-//             menu.innerHTML = allText;
-//         }
-//     }
-// }
-// htmlFile.send(null);
-
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
