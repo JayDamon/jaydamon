6b8e1b0ba7772466501b821b1d8492aa320d15c7
= <Jay.n.damon@gmail.com>
2021-08-13 12:51:22 -0400

    feat(head): Add header style transition on scroll

--- app/index.js
+++ app/index.js
@@ -11,6 +11,12 @@ htmlFile.onreadystatechange = function () {
 }
 htmlFile.send(null);
 
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function() {
+    mainNav.classList.toggle('active');
+})
 
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById('js-navbar-toggle');
@@ -18,3 +24,23 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function() {
     mainNav.classList.toggle('active');
 })
+
+const header = document.querySelector("#menu-component");
+
+const sectionOne = document.querySelector("#about");
+
+const sectionOneOptions = {
+    rootMargin: "-50px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(entries) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            header.classList.add('background-dark');
+        } else {
+            header.classList.remove('background-dark');
+        }
+    })
+}, sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
